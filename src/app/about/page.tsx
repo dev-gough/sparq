fe6590bfb0a539963e591dfe9904189c95846b12
Devon Gough <dgough@sparqsys.com>
2025-04-07 16:49:59 -0400

    finished sec. 1&2 per docs

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -128,6 +128,28 @@ export default function AboutPage() {
     return (
         <div className="bg-white container mx-auto py-8 px-10">
             {/* Team Section */}
+            <section id="2.i)">
+                <h2 className="text-brand-maroon text-2xl">Passion</h2>
+                <p>SPARQ was born out of a passion to create leading edge solar energy solutions that support a greener future for our planet. Founder Dr. Praveen Jain is a world-leader in energy research, holding over 100 patents. His advanced research at ePower, the Centre for Energy and Power Electronics Research at Queen’s University in Kingston, Canada, resulted in the development of SPARQ’s innovative and versatile microinverter design.</p>
+                <p>Today SPARQ’s microinverter system is resetting the industry standard for solar energy systems, delivering greater energy harvest over traditional string inverters. It can be used in any power grid, conventional or smart, around the globe.</p>
+            </section>
+            <section id="2.ii)">
+                <h2 className="text-brand-maroon text-2xl mt-16">Vision</h2>
+                <p className="font-mono capitalize">Needs to be provided</p>
+            </section>
+            <section id="2.iii)">
+                <h2 className="text-brand-maroon text-2xl mt-16">Mission</h2>
+                <p className="font-mono capitalize">Needs to be provided</p>
+            </section>
+            <section id="2.iv)">
+                <h2 className="text-brand-maroon text-2xl mt-16">Value Proposition</h2>
+                <p className="font-mono capitalize">needs to be provided</p>
+            </section>
+            <section>
+                <p>Notes:</p>
+                <p>The maroon text is to be the info shown in the scroller (similar to home page), and the text underneath in black is to be shown via a "see more" button</p>
+                <p>The "Meet the Team" and "Board of Directors" sections below should also be shown when clicking the "see more" button, but keep the structure/style as is.</p>
+            </section>
             <section id="#leadership" className="py-16">
                 <div className="container mx-auto">
                     <h1 className="text-5xl font-bold text-brand-maroon text-center mt-12 mb-32">Meet the Team</h1>
