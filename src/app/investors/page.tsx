b992216def0234f748d34d7163d82e31ef1c112b
Devon Gough <dgough@sparqsys.com>
2025-04-09 15:45:58 -0400

    changes per rev. 2.7

--- src/app/investors/page.tsx
+++ src/app/investors/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import Link from "next/link";
 import InvestorSlider from "@/components/Sliders/InvestorSlider";
 
 interface CardProps {
@@ -47,7 +46,7 @@ const cards = [
 
 export default function InvestorPage() {
     return (
-        <div className="container mx-auto py-16 px-10">
+        <div className="container mx-auto py-16 px-10 flex flex-col">
             <section>
                 <InvestorSlider/>
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-8">
@@ -56,29 +55,7 @@ export default function InvestorPage() {
                     ))}
                 </div>
             </section>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            <section id="4.i)">
-                <h1 className="text-5xl text-center my-8">To be included in the slider:</h1>
-                <p className="text-center">mono font means need content</p>
+            <section id="4.i)" className="items-center justify-center">
                 <h1 className="text-brand-maroon font-mono text-2xl mt-16">News (NYI)</h1>
                 <ul className="list-disc list-inside">
                     <li>Icorporate automated news feeds</li>
@@ -100,28 +77,6 @@ export default function InvestorPage() {
                     <li className="font-mono">CEO letter to shareholders</li>
                     <li className="font-mono">Report?</li>
                     <li className="font-mono">ESG Report</li>
-                </ul>
-            </section>
-            <section id="4.iv)">
-                <h1 className="text-brand-maroon font-mono text-2xl mt-16">Sparq Stock</h1>
-                <ul className="list-disc list-inside">
-                    <li>Sparq stock page with canadian and us tickers, where clicking opens a link opens the yahoo finance page</li>
-                    <li className="font-mono">videos from the investment community</li>
-                </ul>
-            </section>
-            <section id="4.v)">
-                <h1 className="text-brand-maroon text-2xl mt-16">Key Partners</h1>
-                <ul className="list-disc list-inside">
-                    <li><Link href="https://www.queensu.ca/epower/" target="_blank" className="text-blue-500">ePower</Link></li>
-                    <li><Link href="https://www.jiothings.com/" target="_blank" className="text-blue-500">JioThings</Link></li>
-                    <li><Link href="https://www.ril.com/businesses/new-energy-materials" target="_blank" className="text-blue-500">Reliance</Link></li>
-                    <li><Link href="https://www.iljin.co.in/" target="_blank" className="text-blue-500">ILJIN Electronics</Link></li>
-                <h2 className="text-brand-maroon text-xl mt-4">Test Sites:</h2>
-                <ul className="list-disc list-inside">
-                    <li>Queen&apos;s University</li>
-                    <li>St. Lawrence College</li>
-                    <li>Modern Niagara (WIP)</li>
-                </ul>
                 </ul>
             </section>
         </div>
